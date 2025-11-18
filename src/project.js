be89fc62bebf23805c42c3d2f4ea920d3f4e80cd
SHOIYAN <shoiyan9@gmail.com>
2025-11-18 21:13:26 +0300

    refactor: remove methods and expose todos array as public for simplicity

--- src/project.js
+++ src/project.js
@@ -1,10 +1,6 @@
 export default function createProject(project_name) {
-  const todos = [];
   return {
     project_name,
-    addTodo: (todo) => {
-      todos.push(todo);
-    },
-    getTodos: () => [...todos],
+    todos: [],
   };
 }
