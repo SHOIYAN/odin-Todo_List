9c826801ba477dee8ae685165318b11b3a0f1d67
SHOIYAN <shoiyan9@gmail.com>
2025-11-22 23:51:56 +0300

    feat: loadAll projects at the start of index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 import './styles/style.css';
+import {create,loadAll,addTodoToProject} from "./projectController";
+import { loadProjects, saveProjects } from './storage';
+
+const savedProjects = loadProjects();
+loadAll(savedProjects);
